15038e0ea83436c893961e8ba6780f6f101c4c61
Garrett Miller <11058125@uvu.edu>
2026-09-19 12:01:15 -0600

    Update Assigment 3

---
+++ Assignments/Assignment_3/Assignment_3_MILLER.R
@@ -0,0 +1,191 @@
+###########################
+#                         #
+#    Assignment Week 3    #
+#                         # 
+###########################
+
+# Instructions ####
+# Fill in this script with stuff that we do in class.
+# It might be a good idea to include comments/notes as well so you remember things we talk about
+# At the end of this script are some comments with blank space after them
+# They are plain-text instructions for what you need to accomplish.
+# Your task is to write the code that accomplished those tasks.
+
+# Then, make sure to upload this to both Canvas and your GitHub repository
+
+getwd()
+
+
+# Vector operations! ####
+
+# Vectors are 1-dimensional series of values in some order
+1:10 # ':' only works for integers
+10:1 # Backwards
+letters # built-in pre-made vector of a - z
+
+
+
+vector1 <- c(1,2,3,4,5,6,7,8,9,10) # set as an object 
+#vector1 <- c(1,2) overwrites the old vector1
+vector2 <- c(5,6,7,8,4,3,2,1,3,10) 
+vector3 <- letters # letters and LETTERS are built-in vectors
+
+vector1 + 5 #adds 5 to each value in vector 1
+vector2 / 2 #divides each value by 2 in vector 2
+#newvector <- vector2 /2 # generates new object based on the function
+vector1*vector2 #multiplies the values in vector 1 and vector 2
+
+vector3 + 1 # can't add 1 to "a" gives error
+
+
+# Logical expressions (pay attention to these...they are used ALL THE TIME)
+vector1 > 3 #gives true or false based on condition set in code
+vector1 >= 3 # greater than or equal to 3
+vector1 < 5
+vector1 <= 5
+vector1 == 7 # equal to 7 need two equal signs 
+letters == "a"
+letters != "c" #c becomes the only false, not the same as 
+letters %in% c("a","b","c","z") #
+vector1 %in% 1:6
+
+
+# Data Frames ####
+# R has quite a few built-in data sets
+data("iris") # load it like this
+
+# For built-in data, there's often a 'help file'
+?iris
+
+# "Iris" is a 'data frame.' 
+# Data frames are 2-dimensional (think Excel spreadsheet)
+# Rows and columns
+# Each row or column is a vector
+
+
+dat <- iris # can rename the object to be easier to type if you want
+dat 
+# ways to get a peek at our data set
+names(dat) #names of columns
+dim(dat) #dimensions of the data columns and rows 
+head(dat) # first six rows of data 
+head(dat, n=3) #reduces to first three lines
+
+# You can access specific columns of a "data frame" by name using '$'
+dat$Species         #list elements in column species
+dat$Sepal.Length    #list elements in sepal.length column
+
+# You can also use square brackets to get specific 1-D or 2-D subsets of a data frame (rows and/or columns)
+dat[1,1] # [Rows, Columns]
+dat[1:3,5] # rows 1-3 of column 5
+
+vector2[1] #subset of vector 2, first element 
+letters[1:7] #first seven letters
+letters[c(1,3,5,7)]
+letters[26:1] #alphabet backwards
+
+
+# Plotting ####
+
+# Can make a quick plot....just give vectors for x and y axes
+plot(x=dat$Petal.Length, y=dat$Sepal.Length) #generates a scatter plot 
+plot(x=dat$Species, y=dat$Sepal.Length) #with word value generates a box plot
+
+
+# Object "Classes" ####
+
+#check the classes of these vectors
+class(dat$Petal.Length) #numeric value
+class(dat$Species) #categorical value 
+
+# plot() function behaves differently depending on classes of objects given to it!
+
+# Check all classes (for each column in dat)
+str(dat) #structure of an R object 
+
+# "Classes" of vectors can be changed if needed (you'll need to, for sure, at some point!)
+
+# Let's try
+nums <- c(1,1,2,2,2,2,3,3,3,4,4,4,4,4,4,4,5,6,7,8,9)
+class(nums) # make sure it's numeric
+
+# convert to a factor
+as.factor(nums) # show in console
+nums_factor <- as.factor(nums) #assign it to a new object as a factor
+class(nums_factor) # check it
+
+
+# convert numeric to character
+as.character(vector1)
+as.character(vector1) + 5 #produces an error because it is no longer numeric
+
+# convert character to numeric
+as.numeric(vector3)#cannot convert letters to numbers
+
+
+
+
+#check it out
+plot(nums) #generates scatter plot
+plot(nums_factor) #generates a histogram
+# take note of how numeric vectors and factors behave differently in plot()
+
+
+
+
+# Simple numeric functions
+# R is a language built for data analysis and statistics so there is a lot of functionality built-in
+
+max(vector1)
+min(vector1)
+median(vector1)
+mean(vector1)
+range(vector1)
+summary(vector1)
+
+# cumulative functions
+cumsum(vector1)
+cumprod(vector1)
+cummin(vector1)
+cummax(vector1)
+
+# even has built-in statistical distributions (we will see more of these later)
+dbinom(50,100,.5) # probability of getting exactly 50 heads out of 100 coin flips
+
+
+
+
+# YOUR REMAINING HOMEWORK ASSIGNMENT (Fill in with code) ####
+
+# 1.  Get a subset of the "iris" data frame where it's just even-numbered rows
+
+seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
+even_dat <- dat[seq(2,nrow(dat),2),]
+even_dat
+str(even_dat)
+
+# 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
+iris_chr <- data.frame(lapply(iris, as.character), stringsasfactors = FALSE)
+iris_chr
+str(iris_chr)
+
+# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
+
+Sepal.Length <- iris$Sepal.Length
+Sepal.Width <- iris$Sepal.Width
+Sepal.Area <- Sepal.Length*Sepal.Width
+
+# 4.  Add Sepal.Area to the iris data frame as a new column
+iris$Sepal.Area <- Sepal.Area
+iris
+
+# 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
+      # (name it big_area_iris)
+
+big_area_iris <- iris[iris$Sepal.Area > 20, ]
+
+
+# 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
+      # to canvas
+      # I should be able to run your R script and get all the right objects generated
+
